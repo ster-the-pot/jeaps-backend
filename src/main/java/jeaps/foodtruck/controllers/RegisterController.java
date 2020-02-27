3a9caccc8d0b6b5e049491b651e86cb19d9fccb8
Jackson_Odonnell <Jackson_Odonnell@baylor.edu>
2020-02-26 20:13:27 -0600

    got login auth working

--- src/main/java/jeaps/foodtruck/controllers/RegisterController.java
+++ src/main/java/jeaps/foodtruck/controllers/RegisterController.java
@@ -5,6 +5,8 @@ import jeaps.foodtruck.common.user.UserDAO;
 import jeaps.foodtruck.common.user.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 
 @RestController
 @RequestMapping(path="/account")
@@ -16,20 +18,22 @@ public class RegisterController {
 
     @PostMapping(path="/create")
     //consider mapping to UserDTO instead of User
-    public @ResponseBody String addUser(UserDTO user){
+    public @ResponseBody String addUser(@RequestBody UserDTO user){
         this.userRepo.save(user);
         return "Successfully saved user";
     }
 
     @PostMapping(path="/login")
     //consider mapping to UserDTO instead of User
-    public @ResponseBody Object LoginUser(UserDTO user) {
-
+    public @ResponseBody Object LoginUser(@RequestBody UserDTO user) {
+        System.out.println(user.getPassword());
         User login = this.userRepo.findByUsername(user.getUsername());
-        if (login == null || login.getPassword() != user.getPassword()) {
+        System.out.println(login.getPassword());
+        if (login == null || !login.getPassword().equals(user.getPassword())) {
             return "authentication failure";
         }
-        return login;
+        return JWT.create().withAudience(user.getUsername())
+                .sign(Algorithm.HMAC256(user.getPassword()));
     }
 
 }
