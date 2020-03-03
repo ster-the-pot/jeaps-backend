4643c1816124a28d7d90fa9c317d7108b4a3566a
Elizabeth Brighton <egbrighton@gmail.com>
2020-03-03 12:28:56 -0600

    Added DAOTest that doesn't quite work

--- src/main/java/jeaps/foodtruck/common/user/user/User.java
+++ src/main/java/jeaps/foodtruck/common/user/user/User.java
@@ -1,6 +1,7 @@
 package jeaps.foodtruck.common.user.user;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
@@ -81,4 +82,21 @@ public class User {
      * @param password The password to be set for the user
      */
     public void setPassword(String password) { this.password = password; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(username, user.username) &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, name, email, password);
+    }
 }
---
+++ src/test/java/DAOTest/UserDAOTest.java
@@ -0,0 +1,80 @@
+package DAOTest;
+
+import jeaps.foodtruck.common.user.user.User;
+import jeaps.foodtruck.common.user.user.UserDAO;
+import jeaps.foodtruck.common.user.user.UserDTO;
+import jeaps.foodtruck.common.user.user.UserRepository;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.MockitoRule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {UserDAO.class, UserRepository.class})
+//@DataJpaTest
+//@ContextConfiguration()
+//@RunWith(MockitoJUnitRunner.class)
+public class UserDAOTest {
+
+
+    @Autowired
+    //@Mock
+    private UserDAO userDAO;
+
+    //@Rule
+    //public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Test
+    public void saveUserTest() {
+        User user = new User();
+
+        user.setUsername("username");
+        user.setPassword("password");
+        user.setName("name");
+        user.setEmail("email");
+
+        System.out.println(user.getUsername());
+        userDAO.save(user);
+
+        User userTest = userDAO.findByUsername("username");
+        System.out.println(userTest.getUsername());
+       /* assertAll(() -> assertEquals(userTest.getUsername(), user.getUsername()),
+                () -> assertEquals(userTest.getEmail(), user.getEmail()),
+                () -> assertEquals(userTest.getId(), user.getId()),
+                () -> assertEquals(userTest.get))*/
+       assertEquals(user, userTest);
+    }
+
+
+    @Test
+    public void saveUserDTOTest() {
+        UserDTO userDTO = new UserDTO();
+
+        userDTO.setUsername("username2");
+        userDTO.setPassword("password2");
+        userDTO.setName("name2");
+        userDTO.setEmail("email2");
+
+        userDAO.save(userDTO);
+
+        User userTest = userDAO.findByUsername(userDTO.getUsername());
+        assertAll(() -> assertEquals(userTest.getUsername(), userDTO.getUsername()),
+                () -> assertEquals(userTest.getEmail(), userDTO.getEmail()),
+                () -> assertEquals(userTest.getName(), userDTO.getName()),
+                () -> assertEquals(userTest.getPassword(), userDTO.getPassword()));
+    }
+
+
+}
