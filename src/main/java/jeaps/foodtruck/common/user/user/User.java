2eecbb5ec91af150dff0b331e95917ebc31b6733
Jackson O'Donnell <Jackson_Odonnell@baylor.edu>
2020-03-02 23:28:06 -0600

    script for test data + some javadoc

--- src/main/java/jeaps/foodtruck/common/user/user/User.java
+++ src/main/java/jeaps/foodtruck/common/user/user/User.java
@@ -8,52 +8,77 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    protected Integer id;
+    // Auto-generated id to ensure unique users
+    private Integer id;
+    // The unique user/screen name
     @Column(unique=true)
-    protected String username;
-    protected String name;
-    protected String email;
-    protected String password;
+    private String username;
+    // The user's real name
+    private String name;
+    // The user's email
+    private String email;
+    // The user's password
+    //TODO: hash this so that security is at least half-@ssed
+    private String password;
 
+    /**
+     * Returns the user's username
+     * @return the user's username
+     */
+    public String getUsername() { return username; }
 
+    /**
+     * Sets the user's username. Should be used in gathering and creation of user objects, but once set, the
+     *    username should remain constant.
+     * @param username The username to be associated with a user
+     */
+    public void setUsername(String username) { this.username = username; }
 
-    public String getUsername() {
-        return username;
-    }
+    /**
+     * Returns the user's unique ID
+     * @return the user's unique ID
+     */
+    public Integer getId() { return id; }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    /**
+     * Sets the user's ID
+     * @param id The ID to be assigned to the user
+     */
+    public void setId(Integer id) { this.id = id; }
 
-    public Integer getId() {
-        return id;
-    }
+    /**
+     * Returns the user's real name
+     * @return the user's real name
+     */
+    public String getName() { return name; }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+    /**
+     * Sets the user's real name
+     * @param name The name of the user
+     */
+    public void setName(String name) { this.name = name; }
 
-    public String getName() {
-        return name;
-    }
+    /**
+     * Returns the user's email address
+     * @return the user's email address
+     */
+    public String getEmail() { return email; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    /**
+     * Sets the user's email address
+     * @param email The email address to be set for the user
+     */
+    public void setEmail(String email) { this.email = email; }
 
-    public String getEmail() {
-        return email;
-    }
+    /**
+     * Returns the user's password
+     * @return the user's password
+     */
+    public String getPassword() { return password; }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    /**
+     * Sets the user's password
+     * @param password The password to be set for the user
+     */
+    public void setPassword(String password) { this.password = password; }
 }
