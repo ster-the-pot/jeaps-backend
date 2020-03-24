cc9ac2f657f418553f417c725d63c50280f71235
unknown <Jackson_Odonnell@baylor.edu>
2020-03-23 19:44:04 -0500

    merge

--- src/main/java/jeaps/foodtruck/common/user/truck/route/Location.java
+++
@@ -1,9 +0,0 @@
-package jeaps.foodtruck.common.user.truck.route;
-
-import javax.persistence.Embeddable;
-
-@Embeddable
-public class Location {
-    private String longitude;
-    private String latitude;
-}
