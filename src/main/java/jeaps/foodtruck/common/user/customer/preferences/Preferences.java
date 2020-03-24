0f2641d425d2cdeb4ef84b42419c1d104a2d6b6f
Elizabeth Brighton <egbrighton@gmail.com>
2020-03-24 15:33:02 -0500

    Only one fav food

--- src/main/java/jeaps/foodtruck/common/user/customer/preferences/Preferences.java
+++ src/main/java/jeaps/foodtruck/common/user/customer/preferences/Preferences.java
@@ -2,10 +2,7 @@ package jeaps.foodtruck.common.user.customer.preferences;
 
 import jeaps.foodtruck.common.user.customer.food.Food;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 import java.util.List;
 import java.util.Set;
 
@@ -16,8 +13,8 @@ public class Preferences {
     @Id
     private Integer id;
 
-    @ManyToMany
-    private Set<Food> foodPref;
+    @ManyToOne
+    private Food foodPref;
 
     private String proxPref;
 
