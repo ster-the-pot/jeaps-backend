b441bf0531d3dfeba46b91f1d243d88182563a5c
unknown <Jackson_Odonnell@baylor.edu>
2020-04-06 18:16:14 -0500

    Made some progress on testing, added functions to Owner

--- src/main/java/jeaps/foodtruck/common/user/owner/Owner.java
+++ src/main/java/jeaps/foodtruck/common/user/owner/Owner.java
@@ -3,10 +3,14 @@ package jeaps.foodtruck.common.user.owner;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jeaps.foodtruck.common.truck.Truck;
+import jeaps.foodtruck.common.truck.food.Food;
+import jeaps.foodtruck.common.user.customer.Customer;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Owner class holds the ID of an owner
@@ -54,5 +58,67 @@ public class Owner {
      */
     public void setTrucks(List<Truck> trucks) {
         this.trucks = trucks;
+    }
+
+    /**
+     * Gets all of the subscribers of the owner
+     *
+     * @return a Set of subscribed customers
+     */
+    public Set<Customer> getSubscribers(){
+
+        //Make a set to hold the subscribers
+        Set<Customer> subs = new HashSet<Customer>();
+        //Add all the subs to the set
+        for(Truck t : this.getTrucks()){
+            subs.addAll(t.getCustomers());
+        }
+
+        return subs;
+    }
+
+    /**
+     * Gets all of the types of food from the trucks the owner has
+     *
+     * @return a Set of food types
+     */
+    public Set<Food> getFoodTypes(){
+
+        //Make a set to hold the food types
+        Set<Food> foods = new HashSet<>();
+        //Add all the foods to the set
+        for(Truck t : this.getTrucks()){
+            foods.addAll(t.getFood());
+        }
+
+        return foods;
+    }
+
+    /**
+     * Gets all of the types of food from the trucks the owner has
+     *
+     * @return a Set of food types
+     */
+    public Double getAvgRating(){
+        Double average = null;
+        Integer num = 0;
+        for (Truck t : trucks) {
+            if(t.getAvgRating() != null){
+                if(average == null) {
+                    average = t.getAvgRating();
+                } else {
+                    average = average + t.getAvgRating();
+                }
+                num++;
+            }
+        }
+        if(average != null) {
+            average = (average / ((double) num));
+
+            return average;
+        }
+
+        //default average to 0
+        return 0.0;
     }
 }
--- src/test/java/DAOTest/OwnerDAOTest.java
+++ src/test/java/DAOTest/OwnerDAOTest.java
@@ -6,10 +6,15 @@ import jeaps.foodtruck.common.user.owner.Owner;
 import jeaps.foodtruck.common.user.owner.OwnerDAO;
 import jeaps.foodtruck.common.user.owner.OwnerRepository;
 import jeaps.foodtruck.common.user.user.User;
+import jeaps.foodtruck.common.user.user.UserDAO;
+import jeaps.foodtruck.common.user.user.UserRepository;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Map;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,20 +26,30 @@ public class OwnerDAOTest {
     @Autowired
     private OwnerDAO ownerDAO = new OwnerDAO();
 
-    OwnerRepository ownerRepo;
+    @Autowired
+    private UserDAO userDAO = new UserDAO();
 
-    User userTest = new User();
-    Optional<Owner> ownerTest = Optional.of(new Owner());
-    Truck truckTest = new Truck();
+    protected OwnerRepository ownerRepo;
+    protected UserRepository userRepo;
+
+    protected User userTest = new User();
+    protected Optional<Owner> ownerTest = Optional.of(new Owner());
+    protected Truck truckTest = new Truck();
+
     @Before
     public void setUp() {
         ownerRepo = mock(OwnerRepository.class);
         ownerDAO.setOwnerRepo(ownerRepo);
 
+        userRepo = mock(UserRepository.class);
+        userDAO.setUserRepo(userRepo);
+
         userTest.setUsername("username");
         userTest.setPassword("password");
         userTest.setName("name");
         userTest.setEmail("email");
+
+
 
         ownerTest.get().setId(userTest.getId());
 
@@ -44,14 +59,32 @@ public class OwnerDAOTest {
     }
 
     @Test
+    @DisplayName("Test finding an owner by their ID")
     public void findByID() {
-
+        System.out.println(userTest.getUsername());
         when(ownerRepo.findById(ownerTest.get().getId())).thenReturn(ownerTest);
         Optional<Owner> owner = ownerDAO.findById(ownerTest.get().getId());
 
         assertAll(() -> assertEquals(ownerTest.get().getTrucks(), owner.get().getTrucks()),
                 () -> assertEquals(ownerTest.get().getId(), owner.get().getId()));
 
+    }
+
+    @Test
+    @DisplayName("Test getting stats from owner using username")
+    public void getOwnerStatsByName() {
+        System.out.println(userTest.getUsername());
+        //attempt to get the owners stats given a username
+        Map<String, Object> stats = ownerDAO.getOwnerStats(userTest.getUsername());
+
+        //Get all of the owner's subscribers
+
+
+        //validate the output
+        assertAll(() -> assertEquals(ownerTest.get().getTrucks().size(), stats.get("Trucks")),
+                () -> assertEquals(ownerTest.get().getSubscribers().size(), stats.get("Subscribers")),
+                () -> assertEquals(ownerTest.get().getFoodTypes().size(), stats.get("FoodTypes")),
+                () -> assertEquals(ownerTest.get().getAvgRating(), stats.get("AvgTruckRating")));
     }
 
 
